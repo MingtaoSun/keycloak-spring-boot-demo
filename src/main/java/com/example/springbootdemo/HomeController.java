892d20bdafb7eeeb0fcee45494cf8a7a59846734
msun <msun@nla.gov.au>
2019-11-12 14:19:50 +1100

    add jetty3, jetty4 profile

--- src/main/java/com/example/springbootdemo/HomeController.java
+++ src/main/java/com/example/springbootdemo/HomeController.java
@@ -2,9 +2,12 @@ package com.example.springbootdemo;
 
 import java.util.Arrays;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -18,5 +21,10 @@ public class HomeController {
     public String products(Model model){
         model.addAttribute("products", Arrays.asList("Apple", "Banana"));
         return "products";
+    }
+    
+    @PostMapping(path = "/k_logout")
+    public String kLogout(HttpServletRequest request) {
+        return "home";
     }
 }
--- src/main/java/com/example/springbootdemo/SecurityConfig.java
+++ src/main/java/com/example/springbootdemo/SecurityConfig.java
@@ -1,47 +1,84 @@
 package com.example.springbootdemo;
 
 
-
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.account.KeycloakRole;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 @KeycloakConfiguration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@Order(10)
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     public static final String BANJO_SYSADMIN_ROLE = "banjo:sysadmin";
 
     @Autowired
     private KeycloakAuthenticationProvider keycloakAuthenticationProvider;
 
+    private static final String[] PUBLIC_PATHS = {"/", "/error", "k_logout2"};
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http.authorizeRequests()
-        .antMatchers("/").permitAll()
-        .antMatchers("/products").authenticated()
+        .antMatchers(PUBLIC_PATHS).permitAll()
+        .antMatchers("/**").authenticated()
         .and().csrf().disable()
         .logout().logoutUrl("/logout");
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(keycloakAuthenticationProvider);
+        auth.authenticationProvider(new AuthenticationProvider(){
+
+            @Override
+            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                KeycloakAuthenticationToken token = (KeycloakAuthenticationToken) authentication;
+                List<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
+
+                for (String role : token.getAccount().getRoles()) {
+                    grantedAuthorities.add(new KeycloakRole(role));
+                }
+                throw new BadCredentialsException("Invalid username or password");
+                //return new KeycloakAuthenticationToken(token.getAccount(), token.isInteractive(), mapAuthorities(grantedAuthorities));
+            }
+
+            private Collection<? extends GrantedAuthority> mapAuthorities(
+                    Collection<? extends GrantedAuthority> authorities) {
+                return authorities;
+            }
+
+            @Override
+            public boolean supports(Class<?> authentication) {
+                return true;
+            }
+        });
     }
-    
+
     // Look for keycloak.* config items in Spring Boot's yml config files instead of expecting a keycloak.json file on the classpath
     /**
      * Use properties in application.properties instead of keycloak.json
@@ -50,7 +87,7 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
         return new KeycloakSpringBootConfigResolver();
     }
-    
+
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
